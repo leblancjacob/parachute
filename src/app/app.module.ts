1e3df8911b0d9e583e4b6e98046e6205514b94eb
Ben Powell <powell.907@osu.edu>
2019-11-02 16:21:03 -0400

    convert css to scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SchedulerComponent } from './scheduler/scheduler.component';
 
 @NgModule({
   declarations: [
     LandingPageComponent,
-    AppComponent
+    AppComponent,
+    SchedulerComponent
   ],
   imports: [
     
